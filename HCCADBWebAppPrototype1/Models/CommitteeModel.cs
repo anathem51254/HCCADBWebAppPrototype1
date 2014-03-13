763e301e29dbab74af914672e78645f1116d9ea8
anathem51254 <ravel.ford@hotmail.com>
2014-03-13 15:33:59 +1100

    Init Commit

---
+++ HCCADBWebAppPrototype1/Models/CommitteeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HCCADBWebAppPrototype1.Models
+{
+    public class CommitteeModel
+    {
+        [Key]
+        public int CommitteeId { get; set; }
+        
+        [Display(Name="Committee Name")]
+        public string CommitteeName { get; set; }
+
+        [Display(Name="Area Of Health")]
+        public string HealthArea { get; set; }
+
+        [Display(Name="Status")]
+        public int Status { get; set; }
+    }
+
+    public class HCCADbContext : DbContext
+    {
+        public DbSet<CommitteeModel> Committees { get; set; }
+    }
+}
